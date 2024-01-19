47b52c0f8e38eebae193efced005547d7fcedc7f
R00tFSDev <149910442+R00tfsdev@users.noreply.github.com>
2024-01-18 20:48:15 -0500

    Create Root.bash

---
+++ Root.bash
@@ -0,0 +1,12 @@
+return users bash32 data:app set true
+echo "failed"
+echo fsck.minix
+e2freefrag start-stop-daemon
+factor accessdb
+c++filt g++
+c++filt factor
+caller faillog
+factor false
+h2xs fc-scan
+then factor is false set if local +-4 file while task set then taskset for dev 
+bash -c "echo $1"
